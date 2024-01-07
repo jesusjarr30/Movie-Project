d801988daf679df056a158042e84d8d3830c5c43
jesusjarr30 <jesus.jarr.30@gmail.com>
2024-01-06 18:11:14 -0600

    first steps react

---
+++ movieClient/movie-gold-v1/src/componets/Layout.js
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+
+import React from 'react'
+
+const Layout = () => {
+
+
+    return (
+        <div>
+
+
+        </div>
+
+
+    )
+}
+export default Layout
