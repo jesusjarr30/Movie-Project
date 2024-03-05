7a91eb5578644cb28df674747e4a87dd4f0fc5ab
jesusjarr30 <jesus.jarr.30@gmail.com>
2024-03-04 22:57:20 -0600

    Css styles for the header

---
+++ movieClient/movie-gold-v1/src/componets/header/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faVideoSlash } from "@fortawesome/free-solid-svg-icons";
+import { Button } from "react-bootstrap";
+import Container from "react-bootstrap";
+import { Nav } from "react-bootstrap";
+import { Navbar } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+ 
+
+const Header = () => {
+    return (
+        <div>
+
+        </div>
+
+    )
+}
+export default Header
